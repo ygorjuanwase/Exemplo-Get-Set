c81f446c8f580dbafa720f9fdecf13d0e04f09b3
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-06-04 17:27:23 -0300

    aprendizado novo terminado

---
+++ ExemploGetSet/ExemploGetSet/Cachorro.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploGetSet
+{
+    class Cachorro
+    {
+        private string Nome;
+        private string Raca;
+        private double Peso;
+        private double Altura;
+        // guardar determinada informaçao
+        public void SetNome(string nome)
+        {
+            if (Nome.Count() < 3)
+            {
+                throw new Exception("Nome deve conter 3 letras");
+            }
+            Nome = nome;
+            if(Nome.Count() > 40)
+            {
+                throw new Exception("Nome deve conter no máximo 40 letras");
+            }
+            Nome = nome;
+        }
+        public void SetAltura(double altura)
+        {
+            if (altura <= 0)
+                {
+                    throw new Exception("Altura devev ser maior que 0");
+                }
+                if(altura > 2)
+                {
+                    throw new Exception("Altura deve ser menor que 2 metros");
+                }
+                Altura = altura;
+        }
+        public void SetPeso(double peso)
+        {
+            if (peso <= 0)
+            {
+                throw new Exception("peso deve ser maior que 0");
+            }
+            if (peso > 150 )
+            {
+                throw new Exception("peso deve ser menor que 150kg");
+            }
+            Peso = peso;
+        }
+        public void SetRaca(string raca)
+        {
+            if (raca.Count() < 3)
+            {
+                throw new Exception("nome da raca deve conter mais de 3 caracteres");
+            }
+            if (raca.Count() > 40)
+            {
+                throw new Exception("Nome da raca deve conter mais de 40 caracteres");
+            }
+            Raca = raca;
+        }
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public double GetAltura()
+        {
+            return Altura;
+        }
+        public double GetPeso()
+        {
+            return Peso;
+        }
+        public string GetRaca()
+        {
+            return Raca;
+        }
+
+
+        // peso /////min: 0,1 max: 150
+        //raca min: 3 max: 40
+    
+    }
+}
---
+++ ExemploGetSet/ExemploGetSet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploGetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro dogao = new Cachorro();
+            dogao.SetNome("aaaaaaaaaaaaaa");
+            dogao.SetAltura(1.50);
+            dogao.SetPeso (50);
+            dogao.SetRaca  ("rotwailer");
+
+            Console.WriteLine(dogao.GetNome());
+            Console.WriteLine(dogao.GetAltura());
+            Console.WriteLine(dogao.GetPeso());
+            Console.WriteLine(dogao.GetRaca());
+        }
+    }
+}
